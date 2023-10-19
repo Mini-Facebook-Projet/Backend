77f40f454f51cab1566be29c1b7d167795d5eaed
issiaka600 <108539189+issiaka600@users.noreply.github.com>
2023-10-19 17:46:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use('/api/v1/users', userRoutes);
-app.use('/api/v1/posts', authMiddleware ,postRoutes);
+app.use('/api/v1/postes', authMiddleware ,postRoutes);
 app.use('/api/v1/comments', commentRoutes);
 
 const config = require('./config/config.js')
